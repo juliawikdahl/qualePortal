1ce9d15d21d2b3358c1a0fba9302da04f73b1f05
Mary Wikdahl <mary.wikdahl@utb.ecutbildning.se>
2022-09-13 15:29:03 +0200

    #ItemPage

--- src/components/itemComponent.vue
+++ src/components/itemComponent.vue
@@ -1,8 +1,13 @@
 <template>
 <div>
-    <p v-for="item in itemss.Components" :key="item.Id">{{item.Id}}</p>
-    <p v-for="item in itemss.Components" :key="item.Id">{{item.Name}}</p>
-    <p v-for="item in itemss.Components" :key="item.Id">{{item.Description}}</p>
+    <div class="itemCards"  v-for="item in itemss.Components" :key="item.Id">
+       <div class="itemName">{{item.Name}}</div>
+   
+        <div class="itemDes">{{item.Description}}</div>
+      <div><div class="codeTitle">Json <button  class="btn1">Xaml</button>,  <button class="btn2">Kopiera</button></div> <div class="itemIndex">{{item.Index}}</div></div>   
+    
+    </div>
+  
 <!-- <textarea v-model="items.name"></textarea>  -->
 
 </div>
@@ -20,11 +25,44 @@
       }
     },
         
-    // data:() => ({
-    //     items{
-    //         name:""
-    //     }
-    // })
   
 } 
     </script>
+<style>
+.itemCards{
+   margin-left: 5rem;
+   margin-right: 20rem;
+   margin-top: 8rem;
+}
+.itemName{
+   
+    font-weight: 800;
+    font-size: 2rem;
+ 
+  margin-top: 1rem; margin-bottom: 1rem;
+}
+.itemDes{
+    
+  
+ margin-bottom: 1rem;
+}
+.itemIndex{
+    padding: 1rem;
+    background-color: rgb(182, 209, 224);
+    
+    
+    min-height: 10rem;
+    min-width: 30rem;
+  margin-bottom: 0.3rem;
+}
+.codeTitle{
+    color:rgb(94, 108, 116);
+    background-color: rgb(182, 209, 224);
+   padding-left: 1rem;
+    min-width: 30rem;
+ border: 1px solid rgb(147, 169, 182);
+}
+.btn1{
+margin-left: 67rem;
+}
+</style>
