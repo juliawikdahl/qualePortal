f368e04aec0dfc83baea2c55fd9f5b86b1bb231c
Mary Wikdahl <mary.wikdahl@utb.ecutbildning.se>
2022-09-29 12:13:29 +0200

    #fixedProblem

--- src/components/itemComponent.vue
+++ src/components/itemComponent.vue
@@ -75,14 +75,15 @@
         <div class="itemName">{{selectedItem.Name}}</div>
         <div class="itemDes">{{selectedItem.Description}}</div>
       
-
+<div class="hej">
         <div class="codeTitle">Json 
+
         <div class="swap">
             <v-icon @click="showJson=!showJson"> mdi-swap-horizontal </v-icon>
         <v-icon @click="copyToClipboard()"> mdi-content-copy </v-icon>
         </div>
       </div> 
-
+</div>
         <div class="itemIndex"><div v-if="showJson">{{selectedItem.Index}}</div>
         <div v-else> {{selectedItem.xmlCode}}</div></div>
 
@@ -97,49 +98,49 @@
 <style scoped>
 <style>
   
-  .swap {
-   display: flex;
-   justify-content: end;
-  }
+   .swap {
+    display: flex;
+    justify-content: end;
+   }
 
 .itemCards{
-  margin-left: 5rem;
-  margin-right: 23rem;
-  margin-top: 5rem;
-  max-width: 100rem;
-  min-width: 70rem;
-  
+   margin-left: 5rem;
+   margin-right: 23rem;
+   margin-top: 5rem;
+   max-width: 100rem;
+   min-width: 70rem;
+   
 }
 .itemName{
-  
-   font-weight: 800;
-   font-size: 2rem;
-
- margin-top: 1rem; margin-bottom: 1rem;
+   
+    font-weight: 800;
+    font-size: 2rem;
+ 
+  margin-top: 1rem; margin-bottom: 1rem;
 }
 .itemDes{
-   
-
-margin-bottom: 1rem;
+    
+ 
+ margin-bottom: 1rem;
 }
 .itemIndex{
 
-   padding: 1rem;
-   border-radius: 5px;
-   background-color: rgb(182, 209, 224);
-   
-   
-   min-height: 10rem;
-   min-width: 35rem;
- margin-bottom: 0.3rem;
+    padding: 1rem;
+    border-radius: 5px;
+    background-color: rgb(182, 209, 224);
+    
+    
+    min-height: 10rem;
+    min-width: 35rem;
+  margin-bottom: 0.3rem;
 }
 .codeTitle{
- border-radius: 5px;
-   color:rgb(94, 108, 116);
-   background-color: rgb(182, 209, 224);
-  padding-left: 1rem;
-   min-width: 30rem;
-border: 1px solid rgb(147, 169, 182);
+  border-radius: 5px;
+    color:rgb(94, 108, 116);
+    background-color: rgb(182, 209, 224);
+   padding-left: 1rem;
+  
+ border: 1px solid rgb(147, 169, 182);
 }
 .btnConvert{
 margin-left: 66rem;
@@ -147,14 +148,14 @@ margin-right: 0.7rem;
 }
 .btnGit {
 
- border: 1px solid rgb(4, 4, 4);
- margin-right: 10px;
- padding: 5px;
- border-radius: 10px;
+  border: 1px solid rgb(4, 4, 4);
+  margin-right: 10px;
+  padding: 5px;
+  border-radius: 10px;
 }
 .icons {
- display: flex;
- justify-content: end;
+  display: flex;
+  justify-content: end;
 }
 
 
