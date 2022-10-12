d42da9e54d730cb0fc929c343037a0c1bd59ffe6
Mary Wikdahl <mary.wikdahl@utb.ecutbildning.se>
2022-10-12 11:18:28 +0200

    #Style

--- src/components/itemComponent.vue
+++ src/components/itemComponent.vue
@@ -154,26 +154,18 @@ if(this.$route.query.openEdit) {
 
 
 <style scoped>
-  .metadata {
-    padding: 0rem 0.5rem;
-    cursor: pointer;
-  }
-  .metadata:hover {
-    color: blue;
-  }
-
+ 
 .itemCards{
    margin-left: 5rem;
    margin-right: 23rem;
    margin-top: 5rem;
-   max-width: 100rem;
-   min-width: 70rem;
-   
+   max-width: 80rem;
+   min-width: 80rem;  
 }
 .itemName{
     font-weight: 800;
     font-size: 2rem;
-  margin-top: 1rem; margin-bottom: 1rem;
+    margin-top: 1rem; margin-bottom: 1rem;
 }
 .itemDes{
  margin-bottom: 1rem;
@@ -184,7 +176,7 @@ if(this.$route.query.openEdit) {
     background-color: rgb(182, 209, 224); 
     min-height: 10rem;
     min-width: 35rem;
-  margin-bottom: 0.3rem;
+    margin-bottom: 0.3rem;
 }
 .codeTitle{
   padding: 0.3rem;
@@ -193,15 +185,11 @@ if(this.$route.query.openEdit) {
     color:rgb(94, 108, 116);
     background-color: rgb(182, 209, 224);
     padding-left: 1rem;
-  
- border: 1px solid rgb(147, 169, 182);
+   border: 1px solid rgb(147, 169, 182);
 }
-.btnConvert{
-margin-left: 66rem;
-margin-right: 0.7rem;
-}
+
 .swap {
-  margin-left: 63rem;
+  margin-left: 73rem;
 }
 .btnGit {
   color: black;
