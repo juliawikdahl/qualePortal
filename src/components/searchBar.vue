f3ef143cb9470fab070bc8e2bc618e36b96dfb29
Julia Wikdahl <julia_2000@live.se>
2022-10-12 11:43:13 +0200

    #cleaning

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -56,9 +56,7 @@
               spellcheck="false"
      
               
-      ></v-autocomplete>
-             <!-- <input class="searchbar"   type="text" placeholder="Search.. "  /> -->
-           
+      ></v-autocomplete> 
         </div>
        
      
@@ -72,44 +70,6 @@
           height: 52px;
           width: 400px;
         }
-    .searchbar {
-      display: flex;
-      font-size: 15px;
-      margin-top: 50px;
-      margin-left: 150px;
-      height: 52px;
-      min-width: 400px;
-      background-color: white;
-      border: transparent;
-      border-radius: 25px;
-      padding-left: 20px;
-      border: 0px solid;
-      outline: none;
-    
-      
-    }
-    a {
-      text-decoration: none;
-      
-    }
-    
-    ::placeholder {
-   
-      text-decoration: none;
-    
-      
-    }
-    .searchbar:hover {
-     
-     box-shadow: 0 0 8px rgb(152, 150, 150);
-     border: 0px solid;
-     outline:none;
-     border: none;
-    }
-    .search {
-        position: absolute;
-        margin-left: 500px;
-        margin-top: -71px;
-    }
+  
     
       </style>
--- src/components/signinComponent.vue
+++ src/components/signinComponent.vue
@@ -63,164 +63,59 @@ import AddItemModal from './addItemModal.vue';
      <div class="userIcon" style="display: flex;">
         
            
-                  <router-link :to="{ path: '/login' }"> 
-     <button   @click="handleLoginButton(), showModal = true" class="btn"> <v-icon id="userIcon" style="color:white;" @click="showModal = false" >mdi-account-outline </v-icon>  {{getSigninText()}}  </button>
-     </router-link>
+    <router-link :to="{ path: '/login' }"> 
+     <button   @click="handleLoginButton(), showModal = true" class="btn"> <v-icon id="userIcon" style="color:white; font-size: 35px;" @click="showModal = false" >mdi-account-outline </v-icon>  {{getSigninText()}}  </button>
+    </router-link>
        
-  
-     
      <AddItemModal v-if ="loggedIn" />
-     
-
     </div> 
 
+
     <div class="modal-overlay" v-if="showModal" @click="showModal =false"></div>
-      
+     
+        <div class="modal" v-if="showModal"> 
+            
+            <h1 class="h1"  >Login</h1>
         
-   
-<div class="modal" v-if="showModal"> 
-   
 
-<div class="add-form">
-       
-    <h1 class="h1"  >Login</h1>
-  
-
-      <div id="password-error" class="invalid"> <small>The username or password you have entered is invalid. Please try again! </small></div>
-      <v-col cols="12">
-                <v-text-field
-                  label="Username*"
-                  required
-                  v-model="username"
-                ></v-text-field>
+            <div id="password-error" class="invalid"> <small>The username or password you have entered is invalid. Please try again! </small></div>
+            <v-col cols="12">
+                        <v-text-field
+                        label="Username*"
+                        required
+                        v-model="username"
+                        ></v-text-field>
             </v-col>
   
 
-  <v-col cols="12">
-                <v-text-field
-                  label="Password*"
-                  :type="show4 ? 'text' : 'password'"
-                  :append-icon="show4 ? 'mdi-eye' : 'mdi-eye-off'"
-                  @click:append="show4 = !show4"
-                  v-model="password"
-                  required
-                ></v-text-field>
+            <v-col cols="12">
+                        <v-text-field
+                        label="Password*"
+                        :type="show4 ? 'text' : 'password'"
+                        :append-icon="show4 ? 'mdi-eye' : 'mdi-eye-off'"
+                        @click:append="show4 = !show4"
+                        v-model="password"
+                        required
+                        ></v-text-field>
               </v-col>
 
 
          <button id="btn" type="submit" @click="handleSignIn()">Log in</button>
- 
- </div>
    
 
-</div>
+          </div>
 
-</div>
+        </div>
  </template>
  
  <style scoped>
    .userIcon {
    margin-left: 8rem;
    }
-
-    .my-form {
-     top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    position: absolute;
-    display: table;
-    
+   .btn {
+        font-size: 20px;
     }
-    .add {
-   
-    margin-left: 0.4rem;
-    margin-top: 2.7rem;
-    }
-    .invalid  {
-       color: red;
-       margin-top: 1rem;
-       margin-left: 0.8rem;
-    
-    
-       }
-    #password-error {
-       display: none;
-    }
-    .h1 {
-       text-align: center;
-    }
-    #btn {
-    margin-top: 2.3rem;
-    height: 40px;
-    width: 300px;
-    cursor: pointer;
-    border: 1px solid;
-    background-color: rgba(15, 15, 15, 0.867);
-    color: white;
-    margin-left: 100px;
- 
-    }
-
-
-   .btn-add {
-    height: 52px;
-   min-width: 100px;
-   color: black;
-   border-radius: 25px;
-   padding: 1rem 2rem;
-   padding-right: 3rem;
-   margin-top: 30px;
-   margin-right: -3rem;
-   margin-left: 3rem;
-   background-color: white;
-   text-align: center;
-   -webkit-transition: all 0.7s ease;
-   transition: all 0.7s ease;
-   border: transparent;
-   cursor: pointer;
-    
-   }
-    #btn:hover {
-        background-color: white;
-        color: black;
-        
-    }
-    
-    .form {
-      color: black;
-      padding-top: 30px;
-      padding-bottom: 50px;
-      padding-left: 50px;
-      padding-right: 50px;
-      box-shadow: 1px 2px 12px 4px #77515148;
-     
-    }
-    
-
-    
-    .box-1 {
-        margin-bottom: 15px;
-    }
-    label {
-     display: block;
-     font: .9rem 'Fira Sans', sans-serif;
-     margin-bottom: 0.4rem;
- 
- }
- 
- input {
-    border: 1px solid;
-    height: 2rem;
-    width: 100%;
-    
- }
-    input[type='submit'],
-    label {
-     
-     margin-top: 1rem;
- }
-
- .modal-overlay {
+    .modal-overlay {
     position: fixed;
     top: 0;
     left: 0;
@@ -245,5 +140,39 @@ import AddItemModal from './addItemModal.vue';
 
 
    }
+.h1 {
+       text-align: center;
+    }
+    .invalid  {
+       color: red;
+       margin-top: 1rem;
+       margin-left: 0.8rem;
+    
+    
+       }
+    #password-error {
+       display: none;
+    }
+    
+    #btn {
+    margin-top: 2.3rem;
+    height: 40px;
+    width: 300px;
+    cursor: pointer;
+    border: 1px solid;
+    background-color: rgba(15, 15, 15, 0.867);
+    color: white;
+    margin-left: 100px;
+ 
+    }
+
+    #btn:hover {
+        background-color: white;
+        color: black;
+        
+    }
+
+    
+
  
     </style>
